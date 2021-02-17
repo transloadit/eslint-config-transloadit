7c1c95285b661563434e442d456a09d533074a1b
Mikael Finstad <finstaden@gmail.com>
2021-02-17 15:12:37 +0100

    initial

---
+++ index.js
@@ -0,0 +1,59 @@
+module.exports = {
+  extends      : 'airbnb-base',
+  parserOptions: { ecmaVersion: 2020 },
+  env          : {
+    node: true,
+  },
+  rules: {
+    // Selective rules/overrides from standard:
+    semi                         : ['error', 'never'],
+    'space-before-function-paren': ['error', 'always'],
+    'object-curly-newline'       : ['error', { multiline: true, consistent: true }],
+
+    // Override/disable certain airbnb rules:
+    'no-plusplus'         : 0,
+    'max-classes-per-file': 0,
+    'max-len'             : 0,
+    'no-underscore-dangle': [
+      'error', {
+        allow               : [],
+        allowAfterThis      : true,
+        allowAfterSuper     : true,
+        enforceInMethodNames: false,
+      },
+    ],
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message : 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+      },
+      {
+        selector: 'LabeledStatement',
+        message : 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message : '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+    ],
+
+    // transloadit custom rules:
+    'key-spacing': [
+      'error',
+      {
+        multiLine: {
+          beforeColon: false,
+          afterColon : true,
+        },
+        align: {
+          beforeColon: false,
+          afterColon : true,
+          on         : 'colon',
+          mode       : 'strict',
+        },
+      },
+    ],
+    'no-multi-spaces': 0,
+  },
+}
